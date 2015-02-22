15668102933c31d2cc753338431b6f770a1a7268
Hao Hu <ihaohu@gmail.com>
2015-02-21 18:00:01 -0800

    save

--- www/login.js
+++ www/login.js
@@ -3,7 +3,30 @@ function getUser() {
     return localStorage.getItem('user');
 }
 
-function userLogin() {
-    $("#popupLogin").popup("open");
-    //console.log('trigger login');
+function userLogin(un, pw) {
+    $.ajax({
+        url: "http://192.168.25.33:5555/login.php?un=" + un + "&pw=" + pw,
+        success: function(response) {
+            localStorage.removeItem('user');
+            if(response == 'OK') {
+                localStorage.setItem('user', un);
+                updateCaption();
+            } else {
+                $("#dialog_OK_head").html('Information');
+                $("#dialog_OK_body").html('Login Failed');
+                $("#dialog_OK").popup("open");
+            }
+        },
+        complete: function() {
+            hideBubble();
+        }
+    });
+    
+    return false;
+}
+
+
+function userLogout() {
+    localStorage.removeItem('user');
+    updateCaption();
 }
